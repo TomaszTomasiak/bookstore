28a458932aea4a9b79b20daf6eb3ed5204411a21
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-05-26 20:15:17 +0200

    angular added to application

--- src/main/java/com/bookstore/BookController.java
+++ src/main/java/com/bookstore/BookController.java
@@ -1,9 +1,11 @@
 package com.bookstore;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/books/")
 @CrossOrigin(origins = "http://localhost:4200") //since we’re just working locally
 public class BookController {
 
@@ -14,21 +16,20 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/books/")
+    @GetMapping
     public Iterable<Book> getBooks() {
         return bookRepository.findAll();
     }
 
-    @GetMapping("/books/{id}")
+    @GetMapping("{id}")
     public Book getBook(@PathVariable("id") Long id) {
         return bookRepository.findById(id).get();
     }
 
-    @PostMapping("/books/")
+    @PostMapping(consumes= MediaType.APPLICATION_JSON_VALUE)
     public HttpStatus addBook(@RequestBody Book book){
         bookRepository.save(book);
 
         return HttpStatus.CREATED;
     }
-
 }
